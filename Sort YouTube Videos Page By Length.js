12fc2273ee2cd0217c4bed63bcc41fb096025034
Maxwell Gubler <maxwell.j.gubler@gmail.com>
2018-11-09 16:02:57 -0600

    Update Sort YouTube Videos Page By Length v0.2

--- Sort YouTube Videos Page By Length.js
+++ Sort YouTube Videos Page By Length.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Sort YouTube Videos Page By Length
 // @namespace    https://github.com/maxgubler/
-// @version      0.1
+// @version      0.2
 // @description  Loads all videos and sorts by length
 // @author       Max Gubler
 // @match        https://www.youtube.com/*/videos*
 // @grant        none
 // ==/UserScript==
+
+/* TODO: Don't resort if already on sorted page--just reverse order */
+/* TODO: Hide videos while sorting */
+/* TODO: Try to optimize with fetch / getMoreContent */
+// @run-at       document-start
 
 function hmsToSeconds(str) {
     var p = str.split(':'), s = 0, m = 1;
@@ -19,9 +24,12 @@ function hmsToSeconds(str) {
     return s;
 }
 
-function sortAndUpdateDOM() {
+function sortAndUpdateDOM(order) {
+    // Get HTMLCollection of list items
     var liCollection = document.getElementsByClassName('browse-list-item-container');
     var length = liCollection.length;
+
+    // Populate array with objects containing the original position, time length in seconds, and the respective list item element
     var arrayOfObjects = [];
 
     for (var i = 0; i < length; i++) {
@@ -29,9 +37,15 @@ function sortAndUpdateDOM() {
         arrayOfObjects.push({originalPosition: i, time: hmsToSeconds(timeStr), element: liCollection[i]});
     }
 
-    arrayOfObjects.sort(function(a, b){return a.time - b.time});
+    // Sort: 0 is ascending and 1 is descending
+    if (order == 0) {
+        arrayOfObjects.sort(function(a, b){return a.time - b.time});
+    }
+    else if (order == 1) {
+        arrayOfObjects.sort(function(a, b){return b.time - a.time});
+    }
 
-    // Create subnavigation first
+    // Retain the first list item (subnavigation) html
     var subNav = document.querySelector('#browse-items-primary>li');
     var newHTML = subNav.outerHTML;
 
@@ -43,12 +57,12 @@ function sortAndUpdateDOM() {
     document.getElementById('browse-items-primary').innerHTML = newHTML;
 }
 
-function start() {
-    /* Display All Videos */
+// Load all videos
+function start(order) {
     var firstClick = 0;
 
     // Select the node that will be observed for mutations
-    var targetNode = document.getElementById('body');
+    var targetNode = document.body;
 
     // Options for the observer (which mutations to observe)
     var config = { attributes: true, attributeOldValue: true, characterData: true, characterDataOldValue: true, childList: true, subtree: true };
@@ -56,7 +70,6 @@ function start() {
     // Callback function to execute when mutations are observed
     var callback = function(mutationsList, observer) {
         for(var mutation of mutationsList) {
-            //console.log(mutation);
             if (firstClick == 0 && document.querySelector('body.page-loaded')) {
                 firstClick = 1;
                 document.querySelector('.browse-items-load-more-button').click();
@@ -68,7 +81,7 @@ function start() {
                 }
                 else {
                     observer.disconnect();
-                    sortAndUpdateDOM();
+                    sortAndUpdateDOM(order);
                     return;
                 }
             }
@@ -82,13 +95,92 @@ function start() {
     observer.observe(targetNode, config);
 }
 
-/* TODO: Create drop-down button options, sort high to low or low to high and don't resort if already on sorted page--just reverse order */
-/* TODO: Hide videos while sorting */
-/* TODO: Try to optimize with fetch / getMoreContent */
-/* TODO: Refactor out specific functions */
+function fixSubnavList(order){
+    var ytUser = document.querySelector('#appbar-nav>a').attributes.href.value;
+
+    if (order == 0) {
+        // Replace button text with selected sort option
+        document.querySelector('.subnav-sort-menu .yt-uix-button-content').innerText = 'Length (ascending)';
+        // Restore Date added (newest) as a list item
+        document.querySelectorAll('.subnav-sort-menu li')[2].innerHTML = "<span href=\"" + ytUser + "/videos?sort=dd&amp;view=0&amp;flow=list\" onclick=\";yt.window.navigate(this.getAttribute('href'));return false;\" class=\" yt-uix-button-menu-item spf-link\">Date added (newest)</span>"
+    }
+    else if (order == 1) {
+        // Replace button text with selected sort option
+        document.querySelector('.subnav-sort-menu .yt-uix-button-content').innerText = 'Length (descending)';
+        // Restore Date added (newest) and Length (ascending) as a list items
+        document.querySelectorAll('.subnav-sort-menu li')[2].innerHTML = "<span href=\"" + ytUser + "/videos?sort=dd&amp;view=0&amp;flow=list\" onclick=\";yt.window.navigate(this.getAttribute('href'));return false;\" class=\" yt-uix-button-menu-item spf-link\">Date added (newest)</span>"
+        document.querySelectorAll('.subnav-sort-menu li')[3].innerHTML = "<span href=\"" + ytUser + "/videos?sort=la&amp;view=0&amp;flow=list\" onclick=\";yt.window.navigate(this.getAttribute('href'));return false;\" class=\" yt-uix-button-menu-item spf-link\">Length (ascending)</span>"
+    }
+}
+
+function getUrlVar() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function hideListItems() {
+    var li = document.querySelector('#browse-items-primary>li');
+    var hideLi = '<style>.feed-item-container {opacity: 0;}</style>';
+    li.insertAdjacentHTML('beforebegin', hideLi);
+    li.after(document.querySelector('.browse-items-load-more-button'));
+}
+
+function evaluateUrl() {
+    var urlVar = getUrlVar().sort;
+
+    if (urlVar == 'la') {
+        fixSubnavList(0);
+        hideListItems();
+        start(0);
+    }
+    else if (urlVar == 'ld') {
+        fixSubnavList(1);
+        hideListItems();
+        start(1);
+    }
+}
+
+function createMenuOptions() {
+    var ul = document.querySelector('.subnav-sort-menu>ul');
+    var ytUser = document.querySelector('#appbar-nav>a').attributes.href.value;
+
+    ul.innerHTML += "<li role=\"menuitem\"><span href=\"" + ytUser + "/videos?sort=la&amp;view=0&amp;flow=list\" onclick=\";yt.window.navigate(this.getAttribute('href'));return false;\" class=\" yt-uix-button-menu-item spf-link\">Length (ascending)</span></li>";
+    ul.innerHTML += "<li role=\"menuitem\"><span href=\"" + ytUser + "/videos?sort=ld&amp;view=0&amp;flow=list\" onclick=\";yt.window.navigate(this.getAttribute('href'));return false;\" class=\" yt-uix-button-menu-item spf-link\">Length (descending)</span></li>";
+}
+
+function createMenuOnNavigation() {
+    // Select the node that will be observed for mutations
+    var targetNode = document.body;
+
+    // Options for the observer (which mutations to observe)
+    var config = { attributes: true };
+
+    // Callback function to execute when mutations are observed
+    var callback = function(mutationsList, observer) {
+        for(var mutation of mutationsList) {
+            if (mutation.attributeName == 'data-spf-name'){
+                createMenuOptions();
+                evaluateUrl();
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    var observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+}
+
 (function() {
     'use strict';
 
-    start();
+    createMenuOnNavigation();
+    createMenuOptions();
+    // Evaluate and start if sorting by length
+    evaluateUrl();
 
 })();
